77d6b43f0d18edbed613a95eaf6bbecc7bb1c008
Iewandowski <66881874+Iewandowski@users.noreply.github.com>
2024-04-16 19:12:06 -0300

    Delete backend/gambling_control directory

--- backend/gambling_control/src/main/java/com/carol_lewandowski/gambling_control/exception/DefaultException.java
+++
@@ -1,7 +0,0 @@
-package com.carol_lewandowski.gambling_control.exception;
-
-public class DefaultException extends RuntimeException{
-    public DefaultException(String message) {
-        super(message);
-    }
-}
